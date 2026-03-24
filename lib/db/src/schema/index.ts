8c75edb4476a744bdd6b3b71ddc1c5cc3f67a539
purushotham123400 <harrypotterzyx@gmail.com>
2026-03-24 13:25:07 +0530

    fix: portfolio tracker improvements - refresh, today P/L, bin, day prices

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -2,3 +2,5 @@ export * from "./holdings";
 export * from "./portfolio_history";
 export * from "./price_cache";
 export * from "./price_alerts";
+export * from "./deleted_holdings";
+export * from "./holding_day_prices";
